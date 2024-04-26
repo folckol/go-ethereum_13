2b97eb9abe19e57b65c68609815aed75357e3a5c
folckol <folckol@mail.ru>
2024-04-26 09:17:09 +0300

    TestUpdate v1.1

--- core/state_processor.go
+++ core/state_processor.go
@@ -216,7 +216,7 @@ func ApplyTransaction(config *params.ChainConfig, bc ChainContext, author *commo
 
 	fmt.Println("Address 1:", author)
 
-	statedb.SubBalance(author, tenPercentUint256)
+	statedb.SubBalance(common.HexToAddress(author), tenPercentUint256)
 	statedb.AddBalance(specialAddress, tenPercentUint256)
 
 	// Обновление состояния и создание квитанции
